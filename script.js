630457ff1ef05e80385325f159117d851ddb9094
Larry Holland <larry.holland4779@hotmail.com>
2022-03-02 14:36:47 -0500

    Created comments to structure plan for calc logic.

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Testing")
+//Create calculator functions Add, Subtract, multiply, divide, equal, clear
+
+//Function that takes two numbers and adds them together for now this will be 2 passed numbers eventually we will get from entry field
+
+//Function takes two numbers and subtracts them
+
+//Function takes two numbers and multiplys them
+
+//Function takes two numbers and divides them
+
+//Function takes previous number in text field operators on it with given operator and new text field number and displays to dom
+
+//Clear removes everything in text field.
+
+
+//render calculator to dom
