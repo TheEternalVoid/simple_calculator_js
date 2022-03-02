b6512d6613e6a826f24a28f357fc06b51d074f5c
Larry Holland <larry.holland4779@hotmail.com>
2022-03-02 14:41:56 -0500

    Created very basic math functions.

--- script.js
+++ script.js
@@ -2,19 +2,19 @@
 
 //Function that takes two numbers and adds them together for now this will be 2 passed numbers eventually we will get from entry field
 function add(num1, num2) {
-    return 
+    console.log(num1 + num2)
 }
 //Function takes two numbers and subtracts them
 function subtract(num1, num2) {
-    return 
+    console.log(num1 - num2)
 }
 //Function takes two numbers and multiplys them
 function multiply(num1, num2) {
-    return 
+    console.log(num1 * num2)
 }
 //Function takes two numbers and divides them
 function divide(num1, num2) {
-    return 
+    console.log(num1 / num2) 
 }
 //Function takes previous number in text field operators on it with given operator and new text field number and displays to dom
 
@@ -22,3 +22,10 @@ function divide(num1, num2) {
 
 
 //render calculator to dom
+
+
+//Testing functions ** REMOVE ON PROJECT COMPLETION **
+add(1,2);
+subtract(1,2);
+multiply(1,2);
+divide(1,2);
