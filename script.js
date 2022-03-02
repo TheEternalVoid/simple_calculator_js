58ac97f37ecabb13f5ad64367b6890225a1f1a8c
Larry Holland <larry.holland4779@hotmail.com>
2022-03-02 14:39:04 -0500

    Created function outline

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 //Create calculator functions Add, Subtract, multiply, divide, equal, clear
 
 //Function that takes two numbers and adds them together for now this will be 2 passed numbers eventually we will get from entry field
-
+function add(num1, num2) {
+    return 
+}
 //Function takes two numbers and subtracts them
-
+function subtract(num1, num2) {
+    return 
+}
 //Function takes two numbers and multiplys them
-
+function multiply(num1, num2) {
+    return 
+}
 //Function takes two numbers and divides them
-
+function divide(num1, num2) {
+    return 
+}
 //Function takes previous number in text field operators on it with given operator and new text field number and displays to dom
 
 //Clear removes everything in text field.
